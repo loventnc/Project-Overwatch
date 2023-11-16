21b77bb652d87cf11de067692285acc325f57e9a
loventnc <love.nattanischa2003@gmail.com>
2023-11-17 00:01:17 +0700

    pictures slide

--- clients/src/Overview.jsx
+++ clients/src/Overview.jsx
@@ -13,8 +13,16 @@ import TeamDeathMatch from "../src/image/TeamDeathMatch.png";
 import HeoresMenu from "../src/image/HeoresMenu.jpg";
 import MapMenu from "../src/image/MapMenu.jpg";
 import CaptureFlagPIC from "../src/image/CaptureFlagPIC.jpg";
+import ControlPIC from "../src/image/ControlPIC.jpg";
+import DeathmatchPIC from "../src/image/DeathmatchPIC.jpg";
+import EliminationPIC from "../src/image/EliminationPIC.jpg";
+import EscortPIC from "../src/image/EscortPIC.jpg";
+import FlashpointPIC from "../src/image/FlashpointPIC.jpg";
+import HybridPIC from "../src/image/HybridPIC.jpg";
+import PushPIC from "../src/image/PushPIC.jpg";
+import TeamDeathMatchPIC from "../src/image/TeamDeathMatchPIC.jpg";
 import { SlArrowLeft } from "react-icons/sl";
-// import { Carousel } from 'react-responsive-carousel';
+
 
 const Overview = () => {
 
@@ -115,7 +123,93 @@ const Overview = () => {
     </div>
     <SlArrowLeft /> */}
     </div>
+    
+    {/* function pictures() {
+      return (<div>
+        <Carousel />
+      </div>
+      );
+    } */}
 
+    {/* pictures slide */}
+    <div className='bg-[#DDF2FD] pt-10'>
+      <div className="carousel w-full">
+        <div id="slide1" className="carousel-item relative w-full">
+          <img src={CaptureFlagPIC} className="mx-auto" width="700px" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide9" className="btn btn-circle">❮</a> 
+            <a href="#slide2" className="btn btn-circle">❯</a>
+          </div>
+        </div> 
+        <div id="slide2" className="carousel-item relative w-full">
+          <img src={ControlPIC} className="mx-auto" width="700px"/>
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle">❮</a> 
+            <a href="#slide3" className="btn btn-circle">❯</a>
+          </div>
+        </div> 
+        <div id="slide3" className="carousel-item relative w-full">
+          <img src={DeathmatchPIC} className="mx-auto" width="700px" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle">❮</a> 
+            <a href="#slide4" className="btn btn-circle">❯</a>
+          </div>
+        </div> 
+        <div id="slide4" className="carousel-item relative w-full">
+        <img src={EliminationPIC} className="mx-auto" width="700px"/>
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" className="btn btn-circle">❮</a> 
+            <a href="#slide5" className="btn btn-circle">❯</a>
+          </div>
+        </div>
+        <div id="slide5" className="carousel-item relative w-full">
+        <img src={EscortPIC} className="mx-auto" width="700px"/>
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide4" className="btn btn-circle">❮</a> 
+            <a href="#slide6" className="btn btn-circle">❯</a>
+          </div>
+        </div>
+        <div id="slide6" className="carousel-item relative w-full">
+        <img src={FlashpointPIC} className="mx-auto" width="700px"/>
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide5" className="btn btn-circle">❮</a> 
+            <a href="#slide7" className="btn btn-circle">❯</a>
+          </div>
+        </div>
+        <div id="slide7" className="carousel-item relative w-full">
+        <img src={HybridPIC} className="mx-auto" width="700px"/>
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide6" className="btn btn-circle">❮</a> 
+            <a href="#slide8" className="btn btn-circle">❯</a>
+          </div>
+        </div>
+        <div id="slide8" className="carousel-item relative w-full">
+        <img src={PushPIC} className="mx-auto" width="700px"/>
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide7" className="btn btn-circle">❮</a> 
+            <a href="#slide9" className="btn btn-circle">❯</a>
+          </div>
+        </div>
+        <div id="slide9" className="carousel-item relative w-full">
+        <img src={TeamDeathMatchPIC} className="mx-auto" width="700px"/>
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide8" className="btn btn-circle">❮</a> 
+            <a href="#slide1" className="btn btn-circle">❯</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    {/* <script>
+      function showSlide(slideId) {
+        // Hide all slides
+        const slides = document.querySelectorAll('.carousel-item');
+        slides.forEach(slide => slide.classList.add('hidden'));
+        
+        // Show the selected slide
+        document.getElementById(slideId).classList.remove('hidden');
+      }
+    </script> */}
     </>
   )
 }
