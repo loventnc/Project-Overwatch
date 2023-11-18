baf7345cb03eea1b0c4bccc12b29b0f6d96e2c7e
loventnc <love.nattanischa2003@gmail.com>
2023-11-18 19:48:03 +0700

    footer

--- clients/src/Components/Followus.jsx
+++ clients/src/Components/Followus.jsx
@@ -3,16 +3,15 @@ import { RiFacebookCircleFill } from "react-icons/ri";
 import { RiInstagramFill } from "react-icons/ri";
 import { RiYoutubeFill } from "react-icons/ri";
 import { NavLink, Link } from "react-router-dom";
-// import blizzard from "../src/image/blizzard.png";
+import blizzard from "../image/blizzard.png";
 
 function Followus() {
     return (
         <>
-            <footer className='bg-[#43484C]'>
+            <footer className='bg-[#43484C] flex justify-center'>
                 <div className="">
                     <h1 className='font-medium text-[20px] text-center text-white'>FOLLOW US</h1>
                     <div className='flex flex-row justify-center text-white text-[30px] pt-2 gap-5'>
-
                         <Link to="https://www.facebook.com/OverwatchSEA/?brand_redir=292929874198161">
                             <RiFacebookCircleFill />
                         </Link>
@@ -23,7 +22,9 @@ function Followus() {
                             <RiYoutubeFill />
                         </Link>
                     </div>
-
+                    <Link to='https://www.blizzard.com/en-us/'>
+                        <img src={blizzard} className="" alt="" width="80px" />
+                    </Link>
                 </div>
             </footer>
         </>
