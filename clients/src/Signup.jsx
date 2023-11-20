3e23d0706f48631eaddb553a0ac11058f1d73936
Chanchanon Rangpeung <110900547+SuperDoggez@users.noreply.github.com>
2023-11-20 11:39:54 +0700

    Login_Signup_1

---
+++ clients/src/Signup.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function Signup(){
+    return(
+        <div>
+            Signup
+        </div>
+    )
+}
+
+export default Signup
