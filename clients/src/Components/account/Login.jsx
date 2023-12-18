567a46b4b272afe169c03c47e531208703365428
Chanchanon Rangpeung <110900547+SuperDoggez@users.noreply.github.com>
2023-12-18 15:23:55 +0700

    TextInput

--- clients/src/Components/account/Login.jsx
+++ clients/src/Components/account/Login.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import TextInput from '../TextInput';
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+        <TextInput />
+        <TextInput />
+        
+    </div>
   )
 }
 
