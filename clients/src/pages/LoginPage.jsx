6ad2293b9e16f98afdb46a5978cc7b5ddb9db24c
Chanchanon Rangpeung <110900547+SuperDoggez@users.noreply.github.com>
2023-12-19 19:55:17 +0700

    front

--- clients/src/pages/LoginPage.jsx
+++ clients/src/pages/LoginPage.jsx
@@ -33,7 +33,7 @@ export default function LoginPage() {
     }
     return (
         <div>
-            <form onSubmit={login}>
+            <form onSubmit={login} className='flex justify-center'>
                 <input type="text" 
                 placeholder="Username" 
                 value={username} 
